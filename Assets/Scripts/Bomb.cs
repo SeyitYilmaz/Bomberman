31ac7d4aa6d260689d4ef65a4ae4cb68ab333820
Seyit Yılmaz <30367777+SeyitYilmaz@users.noreply.github.com>
2020-01-23 20:33:31 +0300

    Bomba halledildi.

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -8,6 +8,7 @@ public class Bomb : MonoBehaviour
     public GameObject explosionEffect;
     public int explosionValue;
     public LayerMask layermask;
+    public LayerMask breakableWallMask;
 
     private void Start()
     {
@@ -23,18 +24,35 @@ public class Bomb : MonoBehaviour
         for (int i = 0; i < explosionValue; i++)
         {
             RaycastHit hit;
-            Physics.Raycast(transform.position + new Vector3(0, 0.5f, 0), direction, out hit, i, layermask);
+            
+            Physics.Raycast(transform.position + new Vector3(0, 0.5f, 0), direction, out hit, i);
 
             if (!hit.collider)
             {
-                GameObject go = Instantiate(explosionEffect, transform.position+ i*direction, Quaternion.identity);
+                GameObject explosion = Instantiate(explosionEffect, transform.position+ i*direction, Quaternion.identity);
                 Destroy(gameObject);
-                Destroy(go,0.5f);
+                Destroy(explosion,0.5f);
             }
-            else
+            else if (hit.collider.transform.gameObject.CompareTag("uWall"))
             {
-                
+                break;
             }
+            else if (hit.collider.transform.gameObject.CompareTag("dWall"))
+            {
+                Destroy(hit.collider.gameObject);
+            }
+            /* Physics.Raycast(transform.position + new Vector3(0, 0.5f, 0), direction, out hit, i, layermask);
+ 
+             if (!hit.collider)
+             {
+                 GameObject explosion = Instantiate(explosionEffect, transform.position+ i*direction, Quaternion.identity);
+                 Destroy(gameObject);
+                 Destroy(explosion,0.5f);
+             }
+             else
+             {
+                 break;
+             }*/
         }
         
        
